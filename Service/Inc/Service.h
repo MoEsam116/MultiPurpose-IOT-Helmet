0729f4f4f9b17d4a635fb014e5a0ffb18fc59a12
EngMohammed-Ayman <engmohammedaymann@gmail.com>
2023-11-05 17:00:14 +0200

    Defining the Structure of the Rest Files

---
+++ Service/Inc/Service.h
@@ -0,0 +1,19 @@
+/*
+ ******************************************************************************
+ * @file           : Service.h
+ * @Author         :
+ * @brief          : HEADER SERVICE FILE FOR USER
+ * @Date           : Nov 5, 2023
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 .
+ * All rights reserved.
+ ******************************************************************************
+ */
+#ifndef INC_SERVICE_H_
+#define INC_SERVICE_H_
+
+
+
+#endif /* INC_SERVICE_H_ */
